44ae121ca4b14d3ff1090b774bdaefc2d4c9abcb
shafinsiddique <siddiqueshafin20@gmail.com>
2020-08-22 07:04:15 -0400

    identifying fields + adding background tasks.

---
+++ background.js
@@ -0,0 +1,3 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log("button has been pressed")
+})
--- popup.js
+++ popup.js
@@ -1,7 +1,19 @@
-console.log("hello")
-var inputs = document.getElementsByTagName("form")
-console.log(window.location.href)
 
-for (index =0; index<inputs.length; index++){ 
-    console.log(index)
-}
+var forms = document.querySelectorAll("form")
+chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log("working")
+});
+
+// for (x =0; x<forms.length; x++) {
+//     for (y=0; y<forms[x].elements.length; y++) {
+//         element = forms[x].elements[y];
+
+//         try {
+//             element.value = "test"
+//         }
+
+//         catch {
+
+//         }
+//     }
+// }
