6c953f5ced8dcca9696b664f9b6329b14abd420c
shafinsiddique <siddiqueshafin20@gmail.com>
2020-08-22 21:17:29 -0400

    added error handling for key value

--- popup.js
+++ popup.js
@@ -1,6 +1,33 @@
 document.addEventListener('DOMContentLoaded',function() {
     var save = document.getElementById("save");
     save.addEventListener('click', function() {
-        console.log("button was pressed");
+        tryStoreValue()
     })
 })
+
+function tryStoreValue() {
+    var form = document.querySelectorAll("form")[0]
+    var key = form.elements.namedItem("key").value
+    var value = form.elements.namedItem("value").value
+    
+    if (key != "" && value != "" ) {
+        sendOutput(true);
+    }
+
+    else {
+        sendOutput(false)
+    }
+}
+
+function sendOutput(valid) {
+    element = document.getElementById("message")
+    if (valid) {
+        html = "<p>variable has been set</p>"
+    }
+
+    else {
+        html = "<p class='red-font'>name or value cannot be empty</p>" 
+    }
+
+    element.innerHTML = html
+}
